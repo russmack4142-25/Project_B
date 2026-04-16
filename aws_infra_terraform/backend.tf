64d05dfeb02e764983fc3a0673e14a46d86ae4f3
Russ McPherson <mine4142@outlook.com>
2026-04-16 14:43:41 +0200

    minor changes to .gitignore and backend.tf file

--- aws_infra_terraform/backend.tf
+++ aws_infra_terraform/backend.tf
@@ -1,7 +1,9 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   backend "s3" {
     bucket = "dev-terraform-states-project-b"
-    key = "argocd/dev/terraform.tfstate"
+    key = "dev/eks/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt = true
